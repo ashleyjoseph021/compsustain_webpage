a375bfc52adb948dc86e20bf560cd40641c919e9
ashleyjoseph021 <54641995+ashleyjoseph021@users.noreply.github.com>
2020-11-18 22:04:52 -0700

    Update main_script.js

--- script/main_script.js
+++ script/main_script.js
@@ -2,7 +2,7 @@
 var aText = new Array(
 "Computational Sustainability"
 );
-var iSpeed = 50; // time delay of print out
+var iSpeed = 75; // time delay of print out
 var iIndex = 0; // start printing array at this posision
 var iArrLength = aText[0].length; // the length of the text array
 var iScrollAt = 20; // start scrolling up at this many lines
